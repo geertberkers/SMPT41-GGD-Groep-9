f2b58d59616de586738b1ecb973a8a5182d15992
bart-kneepkens <bkneepkens@msn.com>
2015-06-26 13:47:13 +0200

    Settings met Google +

--- GGD_Groep_9/app/src/main/java/smpt/proftaak/ggd/settingsActivity.java
+++ GGD_Groep_9/app/src/main/java/smpt/proftaak/ggd/settingsActivity.java
@@ -1,19 +1,38 @@
 package smpt.proftaak.ggd;
 
+import android.content.Intent;
+import android.content.IntentSender;
 import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.CompoundButton;
 import android.widget.EditText;
 import android.widget.Switch;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.Scopes;
+import com.google.android.gms.common.SignInButton;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.Scope;
+import com.google.android.gms.plus.Plus;
 
 /**
  * Created by BartKneepkens on 25/06/15.
  */
-public class settingsActivity extends ActionBarActivity {
+public class settingsActivity extends ActionBarActivity implements GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener,
+        View.OnClickListener
+{
     boolean postcodeHidden = true;
+    boolean isSignedIn;
+
+    private static final int RC_SIGN_IN = 0;
+    /* Client used to interact with Google APIs. */
+    private GoogleApiClient mGoogleApiClient;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,6 +59,16 @@ public class settingsActivity extends ActionBarActivity {
         locatieInputChanged();
 
 
+        // Build GoogleApiClient with access to basic profile
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .addApi(Plus.API)
+                .addScope(new Scope(Scopes.PROFILE))
+                .build();
+
+        findViewById(R.id.sign_in_button).setOnClickListener(this);
+
     }
 
     private void locatieInputChanged()
@@ -62,6 +91,161 @@ public class settingsActivity extends ActionBarActivity {
             cv.setVisibility(View.VISIBLE);
             ed.setVisibility(View.VISIBLE);
             noGps.setVisibility(View.GONE);
+        }
+    }
+
+    @Override
+    public void onConnected(Bundle bundle) {
+        // onConnected indicates that an account was selected on the device, that the selected
+        // account has granted any requested permissions to our app and that we were able to
+        // establish a service connection to Google Play services.
+        System.out.println("onConnected:" + bundle);
+        mShouldResolve = false;
+
+        // Show the signed-in UI
+        //showSignedInUI();
+        Toast.makeText(getApplicationContext(), "signed in!!!! 1337", Toast.LENGTH_SHORT).show();
+
+        isSignedIn = true;
+
+        //set de button naar 'uitloggen'
+        SignInButton signInButton = (SignInButton) this.findViewById(R.id.sign_in_button);
+        // Find the TextView that is inside of the SignInButton and set its text
+        for (int i = 0; i < signInButton.getChildCount(); i++) {
+            View v = signInButton.getChildAt(i);
+
+            if (v instanceof TextView) {
+                TextView tv = (TextView) v;
+                tv.setText("uitloggen");
+                return;
+            }
+        }
+
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+
+    }
+
+    /* Is there a ConnectionResult resolution in progress? */
+    private boolean mIsResolving = false;
+
+    /* Should we automatically resolve ConnectionResults when possible? */
+    private boolean mShouldResolve = false;
+
+    @Override
+    public void onConnectionFailed(ConnectionResult connectionResult) {
+        // Could not connect to Google Play Services.  The user needs to select an account,
+        // grant permissions or resolve an error in order to sign in. Refer to the javadoc for
+        // ConnectionResult to see possible error codes.
+        System.out.println("onConnectionFailed:" + connectionResult.toString());
+
+        if (!mIsResolving && mShouldResolve) {
+            if (connectionResult.hasResolution()) {
+                try {
+                    connectionResult.startResolutionForResult(this, RC_SIGN_IN);
+                    mIsResolving = true;
+                } catch (IntentSender.SendIntentException e) {
+                    System.out.println("Could not resolve ConnectionResult." +  e.toString());
+                    mIsResolving = false;
+                    mGoogleApiClient.connect();
+                }
+            }
+            else {
+                // Could not resolve the connection result, show the user an
+                // error dialog.
+                //showErrorDialog(connectionResult);
+                Toast.makeText(getApplicationContext(), "error", Toast.LENGTH_SHORT).show();
+            }
+        } else {
+            // Show the signed-out UI
+            //showSignedOutUI();
+            Toast.makeText(getApplicationContext(), "logged out", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    @Override
+    protected void onStart()
+    {
+        super.onStart();
+        mGoogleApiClient.connect();
+    }
+
+    @Override
+    protected void onStop()
+    {
+        super.onStop();
+        mGoogleApiClient.disconnect();
+    }
+
+    @Override
+    public void onClick(View v) {
+
+        if (!isSignedIn) {
+            onSignInClicked();
+        }
+
+        if(isSignedIn)
+        {
+            onSignOutClicked();
+        }
+
+
+    }
+
+    public void onSignInClicked(){
+        // User clicked the sign-in button, so begin the sign-in process and automatically
+        // attempt to resolve any errors that occur.
+        mShouldResolve = true;
+        mGoogleApiClient.connect();
+
+        // Show a message to the user that we are signing in.
+        //mStatusTextView.setText(R.string.signing_in);
+        Toast.makeText(getApplicationContext(), "signing in", Toast.LENGTH_SHORT).show();
+    }
+
+    public void onSignOutClicked()
+    {
+        if (mGoogleApiClient.isConnected()) {
+            Plus.AccountApi.clearDefaultAccount(mGoogleApiClient);
+            mGoogleApiClient.disconnect();
+        }
+
+
+        Toast.makeText(getApplicationContext(), "signed out ", Toast.LENGTH_SHORT).show();
+        isSignedIn = false;
+
+
+        //set de button naar 'uitloggen'
+        SignInButton signInButton = (SignInButton) this.findViewById(R.id.sign_in_button);
+        // Find the TextView that is inside of the SignInButton and set its text
+        for (int i = 0; i < signInButton.getChildCount(); i++) {
+            View v = signInButton.getChildAt(i);
+
+            if (v instanceof TextView) {
+                TextView tv = (TextView) v;
+                tv.setText("inloggen");
+                return;
+            }
+        }
+
+
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        System.out.println("onActivityResult:" + requestCode + ":" + resultCode + ":" + data);
+
+        if (requestCode == RC_SIGN_IN) {
+            // If the error resolution was not successful we should not resolve further.
+            if (resultCode != RESULT_OK) {
+                mShouldResolve = false;
+            }
+
+            mIsResolving = false;
+            mGoogleApiClient.connect();
         }
     }
 }
